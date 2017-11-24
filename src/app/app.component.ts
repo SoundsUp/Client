4ce39b077a192aef06a51b7f8499727e8da83274
Cristiana Man <man.cristiana1@gmail.com>
2017-11-24 21:17:16 +0100

    Added linitial layout, profile inside sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,23 @@ import { User } from "./user.model";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'SoundsUp';
   user: User;
 
   constructor(private appService : AppService) {
+    this.user = {
+      id: 1,
+      avatar: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/pembroke-welsh-corgi-dog-breed-pictures/prance-8.jpg',
+      displayName: 'Corgi Chan',
+      email: 'bark@postman.com',
+      description: 'I like Pina Coladas, and getting caught in the rain\n' +
+      'I´m not much into health food, I am into champagne\n' +
+      'I´ve got to meet you by tomorrow noon, and cut through all this red tape\n' +
+      'At a bar called O\'Malley\'s, where we´ll plan our escape'
+    };
   }
 
   getSomeData() {
